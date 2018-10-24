b235a11b2b68b9e9e53f2f2804c2fac779c9ea58
github <sxf1052566766@163.com>
2018-10-24 15:34:27 +0800

    modify option

--- pytorch_sketch2paints/data/__init__.py
+++ pytorch_sketch2paints/data/__init__.py
@@ -25,7 +25,7 @@ def find_dataset_using_name(dataset_name):
         print("In %s.py, there should be a subclass of BaseDataset with class name that matches %s in lowercase." % (
             dataset_filename, target_dataset_name))
         exit(0)
-
+    print('dataset:', dataset)
     return dataset
 
 
